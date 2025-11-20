a851f7b748e5fa10fed25491eab095309480d16a
kroot loops <132160403+sskriptz@users.noreply.github.com>
2025-11-20 17:29:34 -0500

    Add iterative Fibonacci function and user input
    Implement iterative Fibonacci sequence generator.

--- basicAlgorithms/fibonacci.cpp
+++ basicAlgorithms/fibonacci.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void fibonnaciIt(int terms) {
+    long long a = 0;
+    long long b = 1;
+    vector<long long> sequence = {0, 1};
+
+    for (int i = 0; i < terms-2; i++) {
+        long long new_val = a + b;
+        sequence.push_back(new_val);
+        a = b;
+        b = new_val;
+    }
+
+    for (long long n : sequence) {
+        cout << n << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int input;
+    cout << "How many n fibonnaci terms would you like to generate? ";
+    cin >> input;
+
+    fibonnaciIt(input);
+
+    return 0;
+};
