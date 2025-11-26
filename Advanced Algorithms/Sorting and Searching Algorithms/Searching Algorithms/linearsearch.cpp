60ec28f70774e94628c26a1885d897814c490f0e
kroot loops <132160403+sskriptz@users.noreply.github.com>
2025-11-26 00:12:29 -0500

    Update linearsearch.cpp

--- Advanced Algorithms/Sorting and Searching Algorithms/Searching Algorithms/linearsearch.cpp
+++ Advanced Algorithms/Sorting and Searching Algorithms/Searching Algorithms/linearsearch.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int linearSearch(vector<int> arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == target) {
+            return i;  // return index if found
+        }
+    }
+    return -1; // not found
+}
+
+int main() {
+    vector<int> arr = {4, 7, 1, 9, 5};
+    int target = 9;
+
+    int index = linearSearch(arr, target);
+    if (index != -1) {
+        cout << "Found at index: " << index << endl;
+    } else {
+        cout << "Not found" << endl;
+    }
+}
